bf55f94963f1e0e542ad7a3f31dd4340e89b3a90
chelungungun <33939839+chelungungun@users.noreply.github.com>
2021-05-28 22:13:33 +0300

    Add files via upload

---
+++ loaddata.py
@@ -0,0 +1,52 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+from sklearn.model_selection import KFold
+
+def load_data(dataset_name, folds, rng):
+    X = np.load("multi_label_dataset/" + dataset_name + 'x.npy')
+    Y = np.load("multi_label_dataset/" + dataset_name + 'y.npy')
+    Y = Y.astype(int)
+    Y[Y < 0] = 0
+
+    unlabeled = np.where(Y.sum(-1) < 0)[0]
+    print("Features shape = {}".format(X.shape))
+    print("Label shape = {}".format(Y.shape))
+
+    cv = []
+    kf = KFold(n_splits=folds, shuffle=True, random_state=rng)
+    for train, test in kf.split(X):
+        cv.append(test.tolist())
+
+    try:
+        return (unlabeled, cv, csr_matrix(X), csr_matrix(Y))
+    except:
+        return (unlabeled, cv, X, Y)
+
+def load_data_correct(dataset_name):
+    X = np.load("multi_label_dataset/" + dataset_name + 'x.npy')
+    Y = np.load("multi_label_dataset/" + dataset_name + 'y.npy')
+    Y = Y.astype(int)
+    Y[Y < 0] = 0
+
+    return X, Y
+    #
+    # unlabeled = np.where(Y.sum(-1) < 0)[0]
+    # print("Features shape = {}".format(X.shape))
+    # print("Label shape = {}".format(Y.shape))
+    #
+    # cv = []
+    # kf = KFold(n_splits=folds, shuffle=True, random_state=rng)
+    # for train, test in kf.split(X):
+    #     cv.append(test.tolist())
+    #
+    # try:
+    #     return (unlabeled, cv, csr_matrix(X), csr_matrix(Y))
+    # except:
+    #     return (unlabeled, cv, X, Y)
+
+def load_y(dataset_name):
+    Y = np.load("multi_label_dataset/" + dataset_name + 'y.npy')
+    Y = Y.astype(int)
+    Y[Y < 0] = 0
+
+    return Y
